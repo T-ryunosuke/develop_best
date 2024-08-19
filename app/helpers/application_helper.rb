abe11ada31663d2cbcd19a50de1fb9fb82145375
Ryunosuke <dog.kira@yahoo.ne.jp>
2024-08-19 23:33:22 +0900

    トップページの作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   # 三項演算子を使用(ifの簡略化)
   # 一旦adminとかなし
-  def page_title(title = '')
-    base_title = 'best'
+  def page_title(title = "")
+    base_title = "best"
     title.present? ? "#{title} | #{base_title}" : base_title
   end
 end
