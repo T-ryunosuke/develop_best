1b8ccd231e3eb0523636993314dc18e15fe4e02e
Ryunosuke <dog.kira@yahoo.ne.jp>
2024-08-20 17:54:20 +0900

    ログイン/ログアウト

--- db/schema.rb
+++ db/schema.rb
@@ -14,12 +14,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_20_020430) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
