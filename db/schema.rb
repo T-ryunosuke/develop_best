e38a510716a18c5fcda27824d66391b476e80a14
Ryunosuke <dog.kira@yahoo.ne.jp>
2024-08-21 11:00:42 +0900

    投稿一覧

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_20_020430) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_20_110229) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_categories_on_name", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", limit: 255, null: false
+    t.string "image"
+    t.text "content", null: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_posts_on_category_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -28,4 +47,7 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_20_020430) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name"
   end
+
+  add_foreign_key "posts", "categories"
+  add_foreign_key "posts", "users"
 end
