a397dea6b0a45f42c2f2a31f9064823af74809ad
Ryunosuke <dog.kira@yahoo.ne.jp>
2024-08-23 17:49:03 +0900

    投稿削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "static_pages#top"
   resources :users, only: %i[new create]
-  resources :posts, only: %i[index new create show edit update destroy]
+  resources :posts
   resources :categories, only: %i[new create]
   get "login" => "user_sessions#new"
   post "login" => "user_sessions#create"
