a642d0d127f3fb7fa54d92fbb4618699462f0323
Ryunosuke <dog.kira@yahoo.ne.jp>
2024-09-21 15:23:13 +0900

    動的なOGP設定のための修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,30 @@ module ApplicationHelper
     markdown = Redcarpet::Markdown.new(renderer, autolink: true, tables: true, strikethrough: true, superscript: true)
     markdown.render(text).html_safe
   end
+
+  def default_meta_tags
+    {
+      site: 'best',
+      title: 'best',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'あなたがbestと感じた「もの」「場所」「体験」などを投稿してください。',
+      keywords: 'best, ベスト, ランキング, SNS, レビュー, 投稿',
+      canonical: request.original_url,
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('x-post.png'),# 配置するパスやファイル名によって変更する
+        local: 'ja-JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@dog_kira1215',
+        image: image_url('x-post.png'),# 配置するパスやファイル名によって変更
+      }
+    }
+  end
 end
